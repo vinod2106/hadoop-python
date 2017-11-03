b26f1d353cfb3d36e8f576beb1a5033066e8cf84
vinod2106 <vinod254581@gmail.com>
2017-11-03 15:56:40 +0100

    master commit

---
+++ src/main/python/spark/wordcount.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+from operator import add
+
+from pyspark.sql import SparkSession
+
+class WordCount:
+    spark = SparkSession\
+        .builder\
+        .appName("PythonWordCount")\
+        .getOrCreate()
+
+    inputFile = "D://dev//git//hadoop-python//src//main//resources//spark_notes.txt"
+    lines = spark.read.text(inputFile).rdd.map(lambda r: r[0])
+    counts = lines.flatMap(lambda x: x.split(' ')) \
+                  .map(lambda x: (x, 1)) \
+                  .reduceByKey(add)
+    output = counts.collect()
+    for (word, count) in output:
+        print("%s: %i" % (word, count))
+
+    spark.stop()
+    
+if __name__ == "__main__":
+    pass
