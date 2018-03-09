4ce5ad6c6dd398ef2300cfd01f1353e007d5b9d4
vinod254581 <vinod254581@gmail.com>
2018-03-09 11:40:34 +0100

    Compatibile with python 3

--- src/main/python/examples/Dummy.py
+++ src/main/python/examples/Dummy.py
@@ -8,10 +8,10 @@ class Employee:
         Employee.empCount += 1
    
     def displayCount(self):
-        print "Total Employee %d" % Employee.empCount
+        print("Total Employee %d" % Employee.empCount)
 
     def displayEmployee(self):
-        print "Name : ", self.name, ", Salary: ", self.salary
+        print ("Name : ", self.name, ", Salary: ", self.salary)
 
 if __name__ == '__main__':
     emp = Employee("vinod",50)
